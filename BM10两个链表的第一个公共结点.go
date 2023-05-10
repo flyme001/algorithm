fa9686b33f3cabd1c888b1afb71f65ec58ff58d3
flyme001 <891583770@qq.com>
2023-05-10 21:09:38 +0800

    Update BM10两个链表的第一个公共结点.go

--- BM10两个链表的第一个公共结点.go
+++ BM10两个链表的第一个公共结点.go
@@ -1,3 +1,4 @@
+https://www.nowcoder.com/practice/75e878df47f24fdc9dc3e400ec6058ca?tpId=295&tqId=23286&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj%3Fpage%3D1%26tab%3D%25E7%25AE%2597%25E6%25B3%2595%25E7%25AF%2587%26topicId%3D295
 package main
 
 type sameNode struct {
